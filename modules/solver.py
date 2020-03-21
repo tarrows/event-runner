8f84268ae79e397cd72bc418bf18d316d73fa3d1
c10t <c10t@fa.ke>
2020-03-21 23:44:21 +0900

    define items

--- modules/solver.py
+++ modules/solver.py
@@ -1,4 +1,26 @@
 from dataclasses import dataclass
+from typing import Dict
+
+
+@dataclass
+class Material:
+    material_id: int
+    name: str
+
+
+@dataclass
+class Quest:
+    name: str
+    income: Dict[int, int]
+    consumption: int
+
+
+@dataclass
+class Reward:
+    name: str
+    count: int
+    material_id: int
+    require: int
 
 
 @dataclass
